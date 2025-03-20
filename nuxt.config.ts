d770f3a6da1c45efa41a046d36f5540aec603ae6
BK-222 <letsgetjscoding@gmail.com>
2025-03-20 08:17:23 +0000

    Module order modified.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@nuxt/image', '@nuxtjs/google-fonts'],
+  modules: ['@pinia/nuxt', '@nuxt/image', '@nuxtjs/seo', '@nuxtjs/google-fonts'],
   css: ['@/assets/css/main.css'],
   postcss: {
     plugins: {
@@ -19,3 +19,4 @@ export default defineNuxtConfig({
     }
   }
 })
+
